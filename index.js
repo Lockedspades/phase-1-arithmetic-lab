768c6f8251e12d3330be846b137f8c3333911b66
Alex Sanchez <lockedasanchez@gmail.com>
2024-03-05 13:31:26 -0500

    complete lad(3/5)

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 10;
+let num2 = 6.2;
+let multiply = (num1*num2);
+let random;
+random = (Math.floor(Math.random()*10)+1);
+let num3 = 16;
+let num4 = 4;
+let mod = (num3/num4);
+let set = (1, 3, 15, 17, 20);
+let max = Math.max(set);
